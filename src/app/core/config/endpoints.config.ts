08811cb37628f7bf34fa9acaf48621f9914bdb94
Xulio Xaviert Rojas Teixeira <xulio.rojas@vasscompany.com>
2026-01-05 14:00:17 +0100

    feat: add WebSocket integration for live cryptocurrency price updates

--- src/app/core/config/endpoints.config.ts
+++ src/app/core/config/endpoints.config.ts
@@ -4,5 +4,6 @@ export const ENDPOINTS = {
   ticker: '/api/v3/ticker/24hr',
   price: '/api/v3/ticker/price',
   klines: '/api/v3/klines',
-  exchangeInfo: '/api/v3/exchangeInfo'
+  exchangeInfo: '/api/v3/exchangeInfo',
+  ws_url: 'wss://stream.binance.com:9443/ws',
 };
--- src/app/features/dashboard/service/market.service.ts
+++ src/app/features/dashboard/service/market.service.ts
@@ -1,16 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { API_CONFIG } from '../../../core/config/api.config';
+import { ENDPOINTS } from '../../../core/config/endpoints.config';
+import { webSocket } from 'rxjs/webSocket';
+import { delay, map, retry, throttleTime } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class MarketService {
   // Endpoint público base URL for Binance API
   private readonly BASE_URL = API_CONFIG.binance.baseUrl;
+  private http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
+  // Url de Binance API
+  private readonly WS_URL = `${ENDPOINTS.ws_url}/btcusdt@ticker/ethusdt@ticker/solusdt@ticker/dogeusdt@ticker`;
+
+  constructor() {}
+
+  //Motor de conexión WebSocket y lo almacenamos en un signal
+  private marketStream$ = webSocket(this.WS_URL).pipe(
+    //100ms de pausa para no saturar el renderizado
+    throttleTime(100),
+    map((data: any) => ({
+      symbol: data.s,
+      priceChangePercent: data.P,
+      lastPrice: data.c,
+      highPrice: data.h,
+      lowPrice: data.l,
+      volume: data.v,
+    })),
+    //reconexión automática en caso de fallo.
+    retry({ delay: 3000 })
+  );
+
+  //Exponemos el stream para que otros componentes puedan suscribirse
+  public livePriceUpdate = toSignal(this.marketStream$);
 
   //Obtenemos precio de todas las criptomonedas
   getAllPrice() {
@@ -20,5 +46,4 @@ export class MarketService {
   getPriceBySymbol(symbol: string) {
     return this.http.get(`${this.BASE_URL}/ticker/price?symbol=${symbol}`);
   }
-  
 }
